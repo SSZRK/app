0a04d3b3c6d10912efdffe41cbcb55b8f3df0b86
Prezes2137 <michal.heb.pr@gmail.com>
2024-11-09 17:20:02 +0100

    Add updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,6 +8,11 @@ pub fn run() {
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
+            let handle = app.handle().clone();
+                  tauri::async_runtime::spawn(async move {
+                    update(handle).await.unwrap();
+                  });
+
             let file_menu = SubmenuBuilder::new(app, "Plik")
                 .text("open", "Open")
                 .text("save", "Save")
@@ -61,4 +66,28 @@ pub fn run() {
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
+  if let Some(update) = app.updater()?.check().await? {
+    let mut downloaded = 0;
+
+    // alternatively we could also call update.download() and update.install() separately
+    update
+      .download_and_install(
+        |chunk_length, content_length| {
+          downloaded += chunk_length;
+          println!("downloaded {downloaded} from {content_length:?}");
+        },
+        || {
+          println!("download finished");
+        },
+      )
+      .await?;
+
+    println!("update installed");
+    app.restart();
+  }
+
+  Ok(())
 }
