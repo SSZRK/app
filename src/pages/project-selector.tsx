a32ec26d5be8f900844e92ce4ead5317c1eaa87e
Prezes2137 <michal.heb.pr@gmail.com>
2024-11-13 13:38:06 +0100

    Fix dropdown search

--- src/pages/project-selector.tsx
+++ src/pages/project-selector.tsx
@@ -1,9 +1,10 @@
-import {useNavigate} from "react-router-dom";
-import {FormEvent, useEffect, useState} from "react";
+import { useNavigate } from "react-router-dom";
+import { FormEvent, useEffect, useState } from "react";
 import Loading from "../components/common/loading.tsx";
-import Alert, {AlertProps} from "../components/common/alert.tsx";
-import {callApi, Method} from "../utils/call_api.ts";
-import {Project} from "../utils/types.ts";
+import Alert, { AlertProps } from "../components/common/alert.tsx";
+import { callApi, Method } from "../utils/call_api.ts";
+import { Project } from "../utils/types.ts";
+import DropdownSearch from "../components/common/dropdown-search.tsx";
 
 export default function ProjectSelector() {
     const navigate = useNavigate();
@@ -13,7 +14,6 @@ export default function ProjectSelector() {
 
     const [selectedProject, setSelectedProject] = useState('');
     const [search, setSearch] = useState('');
-    const [showHints, setShowHints] = useState(false);
 
     useEffect(() => {
         getData();
@@ -56,59 +56,24 @@ export default function ProjectSelector() {
         navigate(`/${selectedProject}/login`);
     }
 
-    const selectProject = (projectId: string) => {
-        const project = projects.find((project) => project.id === projectId) as Project;
-        setSearch(project.name);
-        setShowHints(false);
-        setSelectedProject(projectId);
-    }
-
-    const openHints = async () => {
-        await new Promise(resolve => setTimeout(resolve, 100));
-        setShowHints(true);
-    }
-
-    const searchProjects = (text: string) => {
-        setShowHints(true);
-        setSearch(text);
-    }
-
     const closeNotification = () => setAlertData(null);
 
     return (
-        <div onClick={() => setShowHints(false)}
-             className="bg-gray-100 bg-[url(../public/Przejazd.webp)] flex items-center select-none justify-center h-screen">
-            <Loading enabled={loading}/>
-            <Alert alertData={alertData} closeNotification={() => closeNotification()}/>
+        <div
+            className="bg-gray-100 bg-[url(../public/Przejazd.webp)] flex items-center select-none justify-center h-screen">
+            <Loading enabled={loading} />
+            <Alert alertData={alertData} closeNotification={() => closeNotification()} />
             <a href="/about"
-               className="flex absolute right-0 bottom-0 h-32 m-5 opacity-50 hover:opacity-80 transition-opacity duration-100">
-                <img src="/icon-full.png" alt="logo"/>
+                className="flex absolute right-0 bottom-0 h-32 m-5 opacity-50 hover:opacity-80 transition-opacity duration-100">
+                <img src="/icon-full.png" alt="logo" />
             </a>
             <div className="bg-white p-8 rounded-lg shadow-lg max-w-sm w-full">
                 <h2 className="text-2xl font-semibold text-center mb-4">Wybierz projekt</h2>
                 <form onSubmit={formSubmit}>
-                    <div className="mb-6">
-                        <label htmlFor="select"
-                               className="block text-gray-700 text-sm font-semibold mb-2"></label>
-                        <input id="select" onFocus={() => openHints()} autoComplete="off"
-                               value={search} onChange={(e) => searchProjects(e.target.value)}
-                               className="form-input w-full px-4 py-2 border rounded-lg text-gray-700 border-gray-300 focus:ring-blue-500"/>
-                        <div
-                            className={`absolute w-full max-w-80 max-h-52 overflow-y-auto bg-white border border-gray-200 rounded-lg shadow-lg ${showHints ? '' : 'hidden'}`}>
-                            {projects.length === 0 &&
-                                <div className="p-2"><p className="text-sm text-gray-700">Brak wyników</p></div>}
-                            {projects.map((project: Project) => {
-                                if (project.name.toLowerCase().includes(search.toLowerCase()))
-                                    return <div className="p-2 cursor-pointer" onClick={() => selectProject(project.id)}
-                                                key={project.id}>
-                                        <p className="text-sm text-gray-700">{project.name}</p>
-                                    </div>;
-                            })}
-                        </div>
-                    </div>
+                    <DropdownSearch search={search} setSearch={setSearch} selectedElement={selectedProject} setElement={setSelectedProject} hints={projects} noElementsMessage="Nie znaleziono posterunków" />
 
                     <button type="submit"
-                            className="w-full bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Przejdź
+                        className="w-full bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Przejdź
                         dalej
                     </button>
                     <p className="text-gray-600 text-xs text-center mt-4">
